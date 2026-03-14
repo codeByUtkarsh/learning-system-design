4f1916bbec110a4230855123c52e0b6853939736
Utkarsh Srivastava <utkarsh.srivastava@minditsystems.com>
2026-03-14 16:37:12 +0530

    Implemented Factory Pattern

---
+++ src/FactoryPattern/ShapeFactory.java
@@ -0,0 +1,14 @@
+package FactoryPattern;
+
+public class ShapeFactory {
+
+    Shape getShape(String input){
+        return switch (input) {
+            case "CIRCLE" -> new Circle();
+            case "RECTANGLE" -> new Rectangle();
+            case "SQUARE" -> new Square();
+            default -> null;
+        };
+    }
+
+}
